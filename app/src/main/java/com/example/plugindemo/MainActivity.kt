288c66dbd5a039af7293462c7e19ac14866939e0
YudizAndroidPiyush <144028214+YudizAndroidPiyush@users.noreply.github.com>
2023-12-05 17:23:17 +0530

    Update module

--- app/src/main/java/com/example/plugindemo/MainActivity.kt
+++ app/src/main/java/com/example/plugindemo/MainActivity.kt
@@ -1,46 +1,17 @@
 package com.example.plugindemo
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.plugindemo.ui.theme.PluginDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PluginDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Piyush")
-                }
-            }
+        setContentView(R.layout.main_activity)
+
+        findViewById<Button>(R.id.clickButton).setOnClickListener {
+            Log.d("TAG", "onCreate: HEREHREHEHREHR")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PluginDemoTheme {
-        Greeting("Android")
     }
 }
